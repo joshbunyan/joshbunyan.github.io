228b61ac63ca6274b5e61678d238f379f8d64ddd
noellewebsites <73456947+noellewebsites@users.noreply.github.com>
2021-02-01 15:55:57 -0600

    Initial commit of my own website.

---
+++ JCB/JS/JCB.js
@@ -0,0 +1,10 @@
+
+/* Responsive menu function */
+function Condense() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
